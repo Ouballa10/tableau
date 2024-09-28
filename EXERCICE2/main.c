8c2eb63dcaf910f6103cff6a57b8616d541431da
ABDELMOUNAIM <abdelmounaimoub12@gmail.com>
2024-09-28 16:02:14 +0200

    v22

---
+++ EXERCICE2/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ int T [10];
+ int i ;
+ float P, M, S;
+ printf("plz entre des nombre\n");
+ for (i=0;i<10;i++){
+        printf("T[%d]",T[i]);
+
+        scanf("%d",&T[i]);
+
+
+ }
+
+
+
+
+
+
+    return 0;
+}
